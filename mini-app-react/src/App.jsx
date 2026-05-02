7c28e95c16f4f3b2cf62950239189f2d2ccd4372
Đặng Ngọc Kim Khánh <dangngockimkhanh@kimkhanhne.local>
2026-05-02 08:43:17 +0700

    Hoàn tất

--- mini-app-react/src/App.jsx
+++ mini-app-react/src/App.jsx
@@ -24,7 +24,7 @@ function App() {
       <MyNavBar />
 
       <Routes>
-        <Route path="/home" element={<Home />} />
+        {/* <Route path="/home" element={<Home />} /> */}
         <Route path="/" element={<Home />} />
         <Route
           path="/list"
